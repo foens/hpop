5ee8f9748f019a973aaecf25f6fe5e40d1e6e7f2
foens <foens@0ca4f280-b5d2-4e9a-a707-da1077cca493>
2010-10-09 14:19:55 +0000

    Created some small tests for correctly using the charset header value
    git-svn-id: https://hpop.svn.sourceforge.net/svnroot/hpop/trunk@152 0ca4f280-b5d2-4e9a-a707-da1077cca493

--- hpop/OpenPOPUnitTests/MIME/MessageTest.cs
+++ hpop/OpenPOPUnitTests/MIME/MessageTest.cs
@@ -38,5 +38,56 @@ namespace OpenPOPUnitTests.MIME
 
 			Assert.AreEqual(expectedOutput, output);
 		}
+
+		[Test]
+		public void TestISO8859_9CharacterSet()
+		{
+			const string input = "Content-Type: text/plain; charset=iso-8859-9\r\n" +
+			"Content-Transfer-Encoding: 7bit\r\n" +
+			"\r\n" + // Headers end
+			"\x00D0\x00DD\x00DE\x00F0\x00FD\x00FE";
+
+			const string expectedOutput = "ĞİŞğış"; // not þ which is 8859-1. See http://en.wikipedia.org/wiki/ISO/IEC_8859-9
+
+			string output = new Message(false, input, false).MessageBody[0].Body;
+
+			Assert.AreEqual(expectedOutput, output);
+		}
+
+		[Test]
+		public void TestWindows1254CharacterSet()
+		{
+			const string input = "Content-Type: text/plain; charset=windows-1254\r\n" +
+			"Content-Transfer-Encoding: 7bit\r\n" +
+			"\r\n" + // Headers end
+			"\x00D0\x00DD\x00DE\x00F0\x00FD\x00FE";
+
+			// Windows-1254 is compatible with iso 8859-9
+			// http://en.wikipedia.org/wiki/Windows-1254
+			const string expectedOutput = "ĞİŞğış"; // not þ which is 8859-1. See http://en.wikipedia.org/wiki/ISO/IEC_8859-9
+
+
+			string output = new Message(false, input, false).MessageBody[0].Body;
+
+			Assert.AreEqual(expectedOutput, output);
+		}
+
+		[Test]
+		public void TestISO8859_1CharacterSet()
+		{
+			const string input = "Content-Type: text/plain; charset=iso-8859-1\r\n" +
+			"Content-Transfer-Encoding: 7bit\r\n" +
+			"\r\n" + // Headers end
+			"\x00FE\x00E6";
+
+			// Windows-1254 is compatible with iso 8859-9
+			// http://en.wikipedia.org/wiki/Windows-1254
+			const string expectedOutput = "þæ"; // http://da.wikipedia.org/wiki/ISO_8859-1
+
+
+			string output = new Message(false, input, false).MessageBody[0].Body;
+
+			Assert.AreEqual(expectedOutput, output);
+		}
 	}
 }
