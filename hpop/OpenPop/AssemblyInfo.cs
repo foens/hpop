e75c221b87c692bbba0cd59a1bc408fcaac2f328
foens <foens@0ca4f280-b5d2-4e9a-a707-da1077cca493>
2011-01-21 16:04:19 +0000

    Allowing partial trust assemblies to use OpenPop
    git-svn-id: https://hpop.svn.sourceforge.net/svnroot/hpop/trunk@304 0ca4f280-b5d2-4e9a-a707-da1077cca493

--- hpop/OpenPop/AssemblyInfo.cs
+++ hpop/OpenPop/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Security;
 
 // Make sure that the unit tests can access internal classes
 // so that it is possible to test them
@@ -17,3 +18,9 @@ using System.Runtime.CompilerServices;
 
 // The Assembly is compliant to CLS rules
 [assembly: CLSCompliant(true)]
+
+// Allow that assemblies can reference this assembly
+// even though they do not have the FullTrust priviledge.
+// This will not defeat security, since normal security policies
+// like DNS, IO permissions are still in effect
+[assembly: AllowPartiallyTrustedCallers]
