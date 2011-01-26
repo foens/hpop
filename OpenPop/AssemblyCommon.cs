14c9367e384a655b413b18f56d8673f2c0df7358
foens <foens@0ca4f280-b5d2-4e9a-a707-da1077cca493>
2011-01-26 18:26:01 +0000

    Changed documentation license
    git-svn-id: https://hpop.svn.sourceforge.net/svnroot/hpop/trunk@318 0ca4f280-b5d2-4e9a-a707-da1077cca493

--- OpenPop/AssemblyCommon.cs
+++ OpenPop/AssemblyCommon.cs
@@ -4,7 +4,7 @@
 // sure that commen assembly information is shared correctly
 
 // Last part of the version number is the SVN version
-[assembly: AssemblyVersion("2.0.1.317")]
+[assembly: AssemblyVersion("2.0.1.318")]
 
 // This is the configuration when building the assembly
 [assembly: AssemblyConfiguration("Release mode assembly. Built for .NET 2.0")]
