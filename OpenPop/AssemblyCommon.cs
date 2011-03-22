3a007f0eee9c195cb1c665758c2bdfe878872c97
foens <foens@0ca4f280-b5d2-4e9a-a707-da1077cca493>
2011-03-22 12:07:00 +0000

    Getting ready to release 2.0.2
    git-svn-id: https://hpop.svn.sourceforge.net/svnroot/hpop/trunk@340 0ca4f280-b5d2-4e9a-a707-da1077cca493

--- OpenPop/AssemblyCommon.cs
+++ OpenPop/AssemblyCommon.cs
@@ -4,7 +4,7 @@
 // sure that commen assembly information is shared correctly
 
 // Last part of the version number is the SVN version
-[assembly: AssemblyVersion("2.0.1.318")]
+[assembly: AssemblyVersion("2.0.2.340")]
 
 // This is the configuration when building the assembly
 [assembly: AssemblyConfiguration("Release mode assembly. Built for .NET 2.0")]
