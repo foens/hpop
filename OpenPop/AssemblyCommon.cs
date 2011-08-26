7f0fdf0e54d396f28038d64c2bee92c7781faf22
foens <foens@0ca4f280-b5d2-4e9a-a707-da1077cca493>
2011-08-26 09:47:28 +0000

    Releasing OpenPop version 2.0.3
    git-svn-id: https://hpop.svn.sourceforge.net/svnroot/hpop/trunk@358 0ca4f280-b5d2-4e9a-a707-da1077cca493

--- OpenPop/AssemblyCommon.cs
+++ OpenPop/AssemblyCommon.cs
@@ -4,7 +4,7 @@
 // sure that commen assembly information is shared correctly
 
 // Last part of the version number is the SVN version
-[assembly: AssemblyVersion("2.0.2.340")]
+[assembly: AssemblyVersion("2.0.3.358")]
 
 // This is the configuration when building the assembly
 [assembly: AssemblyConfiguration("Release mode assembly. Built for .NET 2.0")]
