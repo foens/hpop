d4134166e225ad68c85943d579234da347414a8f
foens <foens@0ca4f280-b5d2-4e9a-a707-da1077cca493>
2012-04-10 09:55:46 +0000

    Jonathan D. Falconnier reported that Convert.FromBase64String may throw OutOfMemoryException because it handles memory wasteful. His fix has been implemented and hereby committed. Thanks!
    git-svn-id: https://hpop.svn.sourceforge.net/svnroot/hpop/trunk@374 0ca4f280-b5d2-4e9a-a707-da1077cca493

--- OpenPop/Mime/Decode/Base64.cs
+++ OpenPop/Mime/Decode/Base64.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 using OpenPop.Common.Logging;
 
@@ -16,9 +18,41 @@ namespace OpenPop.Mime.Decode
 		/// <returns>A byte array that the base64 string described</returns>
 		public static byte[] Decode(string base64Encoded)
 		{
+			// According to http://www.tribridge.com/blog/crm/blogs/brandon-kelly/2011-04-29/Solving-OutOfMemoryException-errors-when-attempting-to-attach-large-Base64-encoded-content-into-CRM-annotations.aspx
+			// System.Convert.ToBase64String may leak a lot of memory
+			// An OpenPop user reported that OutOfMemoryExceptions were thrown, and supplied the following
+			// code for the fix. This should not have memory leaks.
+			// The code is nearly identical to the example on MSDN:
+			// http://msdn.microsoft.com/en-us/library/system.security.cryptography.frombase64transform.aspx#exampleToggle
 			try
 			{
-				return Convert.FromBase64String(base64Encoded);
+				using (MemoryStream memoryStream = new MemoryStream())
+				{
+					base64Encoded = base64Encoded.Replace("\r\n", "");
+
+					byte[] inputBytes = Encoding.ASCII.GetBytes(base64Encoded);
+
+					using (FromBase64Transform transform = new FromBase64Transform(FromBase64TransformMode.DoNotIgnoreWhiteSpaces))
+					{
+						byte[] outputBytes = new byte[transform.OutputBlockSize];
+
+						// Transform the data in chunks the size of InputBlockSize.
+						const int inputBlockSize = 4;
+						int currentOffset = 0;
+						while (inputBytes.Length - currentOffset > inputBlockSize)
+						{
+							transform.TransformBlock(inputBytes, currentOffset, inputBlockSize, outputBytes, 0);
+							currentOffset += inputBlockSize;
+							memoryStream.Write(outputBytes, 0, transform.OutputBlockSize);
+						}
+
+						// Transform the final block of data.
+						outputBytes = transform.TransformFinalBlock(inputBytes, currentOffset, inputBytes.Length - currentOffset);
+						memoryStream.Write(outputBytes, 0, outputBytes.Length);
+					}
+
+					return memoryStream.ToArray();
+				}
 			} catch (FormatException e)
 			{
 				DefaultLogger.Log.LogError("Base64: (FormatException) " + e.Message + "\r\nOn string: " + base64Encoded);
