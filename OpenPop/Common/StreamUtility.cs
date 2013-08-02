08558823041b49d2d433ebbf2d0568627859352d
Link Goron <linkgoron@gmail.com>
2013-08-03 01:03:52 +0300

    added async client

--- OpenPop/Common/StreamUtility.cs
+++ OpenPop/Common/StreamUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OpenPop.Common
 {
@@ -50,6 +51,48 @@ namespace OpenPop.Common
 			}
 		}
 
+        /// <summary>
+        /// Read a line from the stream.
+        /// A line is interpreted as all the bytes read until a CRLF or LF is encountered.<br/>
+        /// CRLF pair or LF is not included in the string.
+        /// </summary>
+        /// <param name="stream">The stream from which the line is to be read</param>
+        /// <returns>A line read from the stream returned as a byte array or <see langword="null"/> if no bytes were readable from the stream</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="stream"/> is <see langword="null"/></exception>
+        public static async Task<byte[]> ReadLineAsBytesAsync(Stream stream)
+        {
+            if (stream == null)
+                throw new ArgumentNullException("stream");
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                while (true)
+                {
+                    byte[] bytes= new byte[1];
+                    int justRead = await stream.ReadAsync(bytes,0,1);
+                    if (justRead == 0 && memoryStream.Length > 0)
+                        break;
+
+                    // Check if we started at the end of the stream we read from
+                    // and we have not read anything from it yet
+                    if (justRead == 0 && memoryStream.Length == 0)
+                        return null;
+
+                    char readChar = (char)justRead;
+
+                    // Do not write \r or \n
+                    if (readChar != '\r' && readChar != '\n')
+                        memoryStream.WriteByte((byte)justRead);
+
+                    // Last point in CRLF pair
+                    if (readChar == '\n')
+                        break;
+                }
+
+                return memoryStream.ToArray();
+            }
+        }
+
 		/// <summary>
 		/// Read a line from the stream. <see cref="ReadLineAsBytes"/> for more documentation.
 		/// </summary>
@@ -61,5 +104,17 @@ namespace OpenPop.Common
 			byte[] readFromStream = ReadLineAsBytes(stream);
 			return readFromStream != null ? Encoding.ASCII.GetString(readFromStream) : null;
 		}
+
+        /// <summary>
+        /// Read a line from the stream. <see cref="ReadLineAsBytes"/> for more documentation.
+        /// </summary>
+        /// <param name="stream">The stream to read from</param>
+        /// <returns>A line read from the stream or <see langword="null"/> if nothing could be read from the stream</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="stream"/> is <see langword="null"/></exception>
+        public static async Task<string> ReadLineAsAsciiAsync(Stream stream)
+        {
+            byte[] readFromStream = await ReadLineAsBytesAsync(stream);
+            return readFromStream != null ? Encoding.ASCII.GetString(readFromStream) : null;
+        }
 	}
 }
